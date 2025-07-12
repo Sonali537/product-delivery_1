13218e079a1931e8faa9e2a0d2e8daf74b6d7f01
Sonali537 <2021wa86186@wilp.bits-pilani.ac.in>
2025-07-12 17:15:22 +0530

    Update App.java

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from CI Pipeline!");
+        System.out.println("CI Build Successful!");
     }
 }
